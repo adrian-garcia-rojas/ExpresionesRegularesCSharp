5784a76a4d296421818d1a1ed084a6114e5b4d9d
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-02-21 22:24:42 -0600

    Se ha generado ejemplo basico de validacion de enteros con @\d

---
+++ ExpresionesRegularesCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExpresionesRegularesCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\d");
+            if (regex.IsMatch("79798"))
+                Console.WriteLine("si");
+            else Console.WriteLine("no");
+
+            Console.ReadLine();
+
+        }
+    }
+}
